3ac7204e58f23b120686662ad9f9df497faf9557
Vanikoro <Vanikoro@email.com>
2021-08-12 20:03:30 -0500

    first commit

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,27 @@
+const { Router } = require('express');
+const router = Router();
+
+const {
+    renderNoteForm,
+    createNewNote,
+    renderNotes,
+    renderEditForm,
+    updateNotes,
+    deleteNote
+} = require('../controllers/notes.controller');
+
+// New note
+router.get('/notes/add', renderNoteForm);
+router.post('/notes/new-note', createNewNote);
+
+//Get all notes
+router.get('/notes', renderNotes);
+
+//Edit notes
+router.get('/notes/edit/:id', renderEditForm);
+router.put('/notes/edit/:id', updateNotes);
+
+//Delete notes
+router.delete('/notes/delete/:id', deleteNote);
+
+module.exports = router;
